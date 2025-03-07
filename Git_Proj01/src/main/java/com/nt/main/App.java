1942f44c67c0b2046b16dc2db1cb53a69f874e02
jolly <jolly@192.168.1.52>
2025-03-07 12:29:07 +0530

    changes done amount by developer

--- Git_Proj01/src/main/java/com/nt/main/App.java
+++ Git_Proj01/src/main/java/com/nt/main/App.java
@@ -13,5 +13,6 @@ public class App {
         App app= new App();
         System.out.println("Sum:: "+app.sum(10,20));
         System.out.println("Sum amount:: "+app.sum(10,20));
+        //how are you??
     }
 }
